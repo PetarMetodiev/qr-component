226a4f626c7f4e1870b93efb2a6b5422894d0c58
Petar Metodiev <petar.metodiev@gmail.com>
2023-11-08 12:29:23 +0200

    implement the component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import imgUrl from "./assets/image-qr-code.png";
 import "./App.css";
 
 import "@fontsource/outfit/400.css";
 import "@fontsource/outfit/700.css";
 
 function App() {
-  return <>Hello!</>;
+  return (
+    <div className="card-container">
+      <div className="card">
+        <div className="picture-container">
+          <img src={imgUrl} />
+        </div>
+        <div className="text-container">
+          <h2 className="card-title">
+            Improve your front-end skills by building projects
+          </h2>
+          <p className="card-description">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
